3df8182f4717ee68691f118de23c17bf0a358708
Justin Maple <justinamaple@gmail.com>
2019-08-30 11:32:55 -0700

    Formatted, froze literal strings, added guard clause

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,30 +1,32 @@
+# frozen_string_literal: true
+
 class BankAccount
-    attr_reader :name 
-    attr_accessor :balance, :status
-    
-    OPEN = "open"
-    CLOSED = "closed"
-    STARTING_BALANCE = 1000
+  attr_reader :name
+  attr_accessor :balance, :status
 
-    def initialize(name, balance = STARTING_BALANCE, status = OPEN)
-        @name = name 
-        self.balance = balance
-        self.status = status   
-    end 
+  OPEN = 'open'
+  CLOSED = 'closed'
+  STARTING_BALANCE = 1000
 
-    def deposit(amount)
-        self.balance += amount  
-    end 
+  def initialize(name, balance = STARTING_BALANCE, status = OPEN)
+    @name = name
+    self.balance = balance
+    self.status = status
+  end
 
-    def display_balance
-        "Your balance is $#{balance}."
-    end 
+  def deposit(amount)
+    self.balance += amount
+  end
 
-    def valid?
-        balance > 0 && status == OPEN
-    end 
+  def display_balance
+    "Your balance is $#{balance}."
+  end
 
-    def close_account 
-        self.status = CLOSED
-    end 
+  def valid?
+    balance.positive? && status == OPEN
+  end
+
+  def close_account
+    self.status = CLOSED
+  end
 end
--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,39 +1,41 @@
+# frozen_string_literal: true
+
 class Transfer
   attr_reader :sender, :receiver, :amount
   attr_accessor :status
-  
-  PENDING = "pending"
-  COMPLETE = "complete"
-  REJECTED = "rejected"
-  REVERSED = "reversed"
+
+  PENDING = 'pending'
+  COMPLETE = 'complete'
+  REJECTED = 'rejected'
+  REVERSED = 'reversed'
 
   def initialize(sender, receiver, amount, status = PENDING)
-    @sender = sender 
-    @receiver= receiver
-    @amount = amount 
-    self.status = status 
-  end 
+    @sender = sender
+    @receiver = receiver
+    @amount = amount
+    self.status = status
+  end
 
   def valid?
-      sender.valid? && receiver.valid?
-  end 
+    sender.valid? && receiver.valid?
+  end
 
-  def execute_transaction 
-    if valid? && sender.balance > amount  && status == PENDING
+  def execute_transaction
+    if valid? && sender.balance > amount && status == PENDING
       sender.balance -= amount
-      receiver.balance += amount 
+      receiver.balance += amount
       self.status = COMPLETE
     else
       self.status = REJECTED
-      "Transaction rejected. Please check your account balance."
+      'Transaction rejected. Please check your account balance.'
     end
   end
 
   def reverse_transfer
-    if status == COMPLETE
-      sender.balance += amount
-      receiver.balance -= amount
-      self.status = REVERSED
-    end   
-  end 
+    return unless status == COMPLETE
+
+    sender.balance += amount
+    receiver.balance -= amount
+    self.status = REVERSED
+  end
 end
